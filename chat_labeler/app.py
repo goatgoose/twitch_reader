ee3cba239cd81d1bf9a884baa5815b4fe9c11f71
Sam Clark <sam.clark1142@gmail.com>
2020-11-18 03:05:45 -0500

    remember last labeled message on restart

--- chat_labeler/app.py
+++ chat_labeler/app.py
@@ -2,13 +2,26 @@ from flask import Flask, render_template, request, redirect
 import sys
 import json
 
-sys.path.append("../twitch_reader")
+sys.path.append("..")
 from chat_file import ChannelFile
-hopped_messages = ChannelFile("../twitch_reader/hopped_messages.json", custom_json=True)
-labeled_path = "../twitch_reader/hand_labeled.json"
+hopped_messages = ChannelFile("../hopped_messages.json", custom_json=True)
 
-for _ in range(5000):
-    hopped_messages.next()
+labeled_path = "../hand_labeled.json"
+labeled_file = ChannelFile(labeled_path, custom_json=True)
+
+last_labeled = None
+while True:
+    next_message = labeled_file.next()
+    if not next_message:
+        break
+    last_labeled = next_message
+
+while True:
+    next_message = hopped_messages.next()
+    if not next_message:
+        break
+    if next_message.timestamp >= last_labeled.timestamp:
+        break
 
 app = Flask(__name__)
 
